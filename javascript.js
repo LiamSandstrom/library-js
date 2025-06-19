96bbae67f88f55a9a44e4a219077fe1551c93968
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-06-19 10:15:12 +0200

    git init and js skeleton

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+const myLibrary = [];
+
+function Book(author, year, pages, stars){
+
+    if(!new.target){
+        throw Error("Book needs to be called with 'new' keyword");
+    }
+    this.author = author;
+    this.year = year;
+    this.pages = pages;
+    this.stars = stars;
+}
+
+function addBookToLibrary(author, year, pages, stars){
+
+    const id = crypto.randomUUID();
+    const newBook = new Book(author, year, pages, stars);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary("bob", 2022, 222, 8);
+addBookToLibrary("leroy", 2010, 37, 7);
+addBookToLibrary("can win", 2025, 777, 10);
+console.log(myLibrary)
+
+function displayBooks(){
+    for(book in myLibrary){
+        
+    }
+}
